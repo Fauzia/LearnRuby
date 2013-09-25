a5c6730271e512d7f231e8c300fe1674ce5c54f9
unknown <muholofauzia@gmail.com>
2013-09-25 23:03:32 +0300

    test7.rb

---
+++ trap.rb
@@ -0,0 +1,7 @@
+height = 3
+a = 6
+b = 8
+area = 0.5*(a + b) *height
+
+
+puts "the are of the trap is %.0f, height is %d, a is %d, b is %d" %[area,height,a,b]
---
+++ triangle.rb
@@ -0,0 +1,11 @@
+height = 10
+base = 5
+area = 0.5 * height * base
+
+# String formatting
+
+puts "The height of the triangle is %d and the base is %d the area is %.1f" % [height,base,area] 
+
+# String interpolation
+
+ puts "The height of the triangle is #{height} and the base is #{base} the area is #{area}"
